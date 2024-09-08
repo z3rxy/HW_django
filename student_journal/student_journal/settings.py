caf1ae5e4b6213d14ce93a350a94b48871615b4a
z3rxy <pa.volkogonov@gmail.com>
2024-09-08 15:24:03 +0300

    minor changes

--- student_journal/student_journal/settings.py
+++ student_journal/student_journal/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y54bd35dt88x!e7vatmc08gfbx2lkso5o_^o-_#!l7=4%3vca1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
